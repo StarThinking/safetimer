4acf2d8f0bc4e94aa796df1cdbf17d990709d70e
StarThinking <sixiangma0408@foxmail.com>
2017-02-16 17:04:52 -0500

    create receiver.c file

--- hb-receiver/socket-server/client.c
+++ hb-receiver/socket-server/client.c
@@ -1,44 +1,49 @@
-#include<stdio.h> //printf
-#include<string.h>    //strlen
-#include<sys/socket.h>    //socket
-#include<arpa/inet.h> //inet_addr
+#include <stdio.h> 
+#include <string.h>    
+#include <sys/socket.h> 
+#include <arpa/inet.h> 
 #include <time.h>
 #include <stdlib.h>
 #include <errno.h>
 #include <sys/types.h>
 #include <unistd.h>
-#include<signal.h>
+#include <signal.h>
 
 #define MSGSIZE 10
-#define INETRVAL 200*1000*1000 // 200ms
+#define TIMEOUT_INTVL 10 // 10s
 
-int sock;
-void sig_handler(int signo)
-{
+int sock = 0;
+
+void sig_handler(int signo) {
       if (signo == SIGINT) {
           printf("client ternimates and close conn\n");
           close(sock);
       }
 }
 
-int main(int argc , char *argv[])
-{
-    signal(SIGINT, sig_handler);
+struct timespec sleep_time() {
+    struct timespec ts;
+    ts.tv_sec = TIMEOUT_INTVL;
+    ts.tv_nsec = 0;
+    return ts;
+}
 
-    if(argc!=2){
+int main(int argc , char *argv[]) {
+
+    signal(SIGINT, sig_handler);
+    
+    if(argc!=2) {
 	printf("client [ip]\n");
 	return -1;
     }
     char *ip = argv[1];
-    printf("ip=%s, msg_size=%d\n", ip, MSGSIZE);
+    printf("ip = %s, msg_size = %d\n", ip, MSGSIZE);
 
     struct sockaddr_in server;
     char *buf = malloc(MSGSIZE); 
-
-    sock = socket(AF_INET , SOCK_STREAM , 0);
-    if (sock == -1)
-    {
-        printf("Could not create socket");
+    sock = socket(AF_INET, SOCK_STREAM , 0);
+    if (sock == -1) {
+        printf("Could not create socket\n");
     }
     puts("Socket created");
      
@@ -46,32 +51,25 @@ int main(int argc , char *argv[])
     server.sin_family = AF_INET;
     server.sin_port = htons(5001);
 
-    if (connect(sock , (struct sockaddr *)&server , sizeof(server)) < 0)
-    {
+    if (connect(sock, (struct sockaddr *) &server , sizeof(server)) < 0) {
         perror("connect failed. Error");
         return 1;
     }
      
     puts("Connected\n");
     
-    struct timespec ts;
-    ts.tv_sec = 0;
-    ts.tv_nsec = INETRVAL; //200ms
-
-    while(1)
-    {   
+    while(1) {   
         int ret = send(sock, buf, MSGSIZE, 0);
-        if(ret <= 0){
+        if(ret <= 0) {
             break;
         }
-        if(ret != MSGSIZE) printf("warning write ret=%d\n", ret);
-	ret = recv(sock, buf, MSGSIZE, 0);
-        if(ret <= 0){
-            break;
-        }
-        if(ret != MSGSIZE) printf("warning recv ret=%d\n", ret);
-        
-        nanosleep(&ts, NULL); // sleep 
+        if(ret != MSGSIZE) 
+            printf("warning write ret=%d\n", ret);
+       
+        // sleep
+        struct timespec sleep_ts = sleep_time();
+        nanosleep(&sleep_ts, NULL); 
     }
+
     return 0;
 }
---
+++ hb-receiver/socket-server/receiver.c
@@ -0,0 +1,138 @@
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <time.h> 
+#include <pthread.h>
+#include <signal.h>
+#include <stdbool.h>
+#include <semaphore.h>
+
+#define MSGSIZE 10
+#define TIMEOUT_INTVL 10 // 10s
+
+static bool expired = true;
+static int local_client_sock;
+
+static pthread_mutex_t lock;
+static sem_t sem;
+
+long now() {   
+    struct timespec spec;    
+    clock_gettime(CLOCK_REALTIME, &spec);
+    return spec.tv_sec * 1000 + spec.tv_nsec/1.0e6;
+}
+
+long next_timeout(long now_ms) {   
+    return now_ms + TIMEOUT_INTVL*1000;
+}
+
+struct timespec sleep_time(long sleep_t) {
+    struct timespec sleep_ts;
+    sleep_ts.tv_sec = sleep_t / 1000;
+    sleep_ts.tv_nsec = (sleep_t % 1000) * 1.0e6;
+    return sleep_ts;
+}
+
+void *expirator(void *arg) {
+    int sock = *(int *) arg;
+    printf("create expirator thread, local client sock = %d\n", sock);
+    long timeout_t;
+    struct timespec sleep_ts;
+    timeout_t = next_timeout(now());
+    while(1) {
+        while(now() < timeout_t) {
+            sleep_ts = sleep_time(timeout_t - now());
+            nanosleep(&sleep_ts, NULL);
+        }
+        timeout_t = next_timeout(now());
+        
+        // send a packet to self
+        char *buf = malloc(MSGSIZE);
+        int ret = send(sock, buf, MSGSIZE, 0);
+        if(ret != MSGSIZE) {
+            printf("write incorrectly, ret = %d\n", ret);
+            break;
+        }
+
+        // do expiration check
+        printf("it's time to check expiration\n");
+        pthread_mutex_lock(&lock);
+        if(expired == true) {
+            printf("expired!\n");
+        } else {
+            expired = true;
+        }
+        pthread_mutex_unlock(&lock);
+    }
+}
+
+void *receiver(void *arg) {
+    char *buf = malloc(MSGSIZE);
+    int connfd = *(int *) arg;
+    while(1) {
+	int ret = recv(connfd, buf, MSGSIZE, 0);
+	if(ret <= 0) {
+	    close(connfd);
+	    break;
+	}
+	if(ret != MSGSIZE) printf("warning recv ret=%d\n", ret);
+        
+        // update
+        if(connfd != local_client_sock)
+            printf("remote heartbeat received, sock = %d\n", connfd);
+        else
+            printf("local heartbeat received, sock = %d\n", connfd);
+        pthread_mutex_lock(&lock);
+        expired = false;
+        pthread_mutex_unlock(&lock);
+    } 
+}
+
+int main(int argc, char *argv[]) {
+    if(argc != 1) {
+	return 1;
+    }
+    int listenfd = 0, connfd = 0;
+    struct sockaddr_in serv_addr; 
+//    sem_init(&sem, 0, 1); 
+
+    listenfd = socket(AF_INET, SOCK_STREAM, 0);
+    memset(&serv_addr, '0', sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    serv_addr.sin_port = htons(5001); 
+    bind(listenfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)); 
+    listen(listenfd, 10); 
+
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
+    if(connect(sock, (struct sockaddr *) &serv_addr , sizeof(serv_addr)) < 0) {
+        perror("connect failed. Error");
+        return 1;
+    } else {
+        printf("connected to self\n");
+    }
+
+    pthread_t expirator_tid;
+    pthread_create(&expirator_tid, NULL, expirator, &sock);
+
+    while(1) {
+        struct sockaddr_in client;
+        unsigned int client_len = sizeof(client);
+        connfd = accept(listenfd, (struct sockaddr*) &client, &client_len);
+        printf("client connection accepted, sock = %d, ip = %s\n", connfd, inet_ntoa(client.sin_addr));
+        // determine which is the local client sockfd
+        if(strcmp(inet_ntoa(client.sin_addr), "127.0.0.1") == 0) {
+            local_client_sock = connfd;
+        }
+	int *tmp = malloc(sizeof(int));
+	*tmp = connfd;
+	pthread_t receiver_tid;
+	pthread_create(&receiver_tid, NULL, receiver, tmp);
+    }
+}
