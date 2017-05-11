1a71dab52bf74cf8d05eb1d3acea722ff321d28f
StarThinking <sixiangma0408@foxmail.com>
2017-05-10 22:00:41 -0400

    add utility

---
+++ hb-queue-receiver/include/utility.h
@@ -0,0 +1,5 @@
+long now();
+
+struct timespec sleep_time(long sleep_t);
+
+long random_at_most(long max);
--- hb-queue-receiver/src/hb_queue.c
+++ hb-queue-receiver/src/hb_queue.c
@@ -17,6 +17,7 @@
 #include <semaphore.h>
 #include <stdbool.h>
 
+#include "utility.h"
 #include "hashtable.h"
 #include "hb_config.h"
 
@@ -71,12 +72,6 @@ static void clear_up(int signo) {
         exit(0);
 }
 
-long now() {
-        struct timespec spec;
-        clock_gettime(CLOCK_REALTIME, &spec);
-        return spec.tv_sec * 1000 + spec.tv_nsec/1.0e6;
-}
-
 // return epoch id
 long round_to_epoch(long time) {
         time -= base_time;
@@ -85,40 +80,6 @@ long round_to_epoch(long time) {
 
 long round_to_interval(long id) {
         return base_time + (id * expiration_interval);
-}
-
-struct timespec sleep_time(long sleep_t) {
-        struct timespec sleep_ts;
-        sleep_ts.tv_sec = sleep_t / 1000;
-        sleep_ts.tv_nsec = (sleep_t % 1000) * 1.0e6;
-        return sleep_ts;
-}
-
-        // sleep [expiration_interval, 2*expiration_interval]
-        /*long rand_sleep_time = random_at_most(expiration_interval);
-        if(rand_sleep_time < 0.1 * expiration_interval) {
-                struct timespec sleep_ts = sleep_time(rand_sleep_time + expiration_interval);
-                printf("[Receiver] Sleep %ld ms.\n", rand_sleep_time + expiration_interval);
-                nanosleep(&sleep_ts, NULL);
-        }*/
-              
-long random_at_most(long max) {
-        unsigned long
-        // max <= RAND_MAX < ULONG_MAX, so this is okay.
-        num_bins = (unsigned long) max + 1,
-        num_rand = (unsigned long) RAND_MAX + 1,
-        bin_size = num_rand / num_bins,
-        defect   = num_rand % num_bins;
-
-        long x;
-        do {
-                x = random();
-        }
-        // This is carefully written not to overflow
-        while (num_rand - defect <= (unsigned long)x);
-
-        // Truncated division is intentional
-        return x/bin_size;
 }
 
 /*
